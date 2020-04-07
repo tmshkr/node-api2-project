50780060ae9391c87b468d4b22096d4e8577a780
Tim Shaker <tim@tmshkr.com>
2020-04-07 12:44:27 -0700

    POST /api/posts/:id/comments

--- router.js
+++ router.js
@@ -17,7 +17,31 @@ router.post("/", (req, res) => {
       })
     );
 });
-router.post("/:id/comments", (req, res) => {});
+
+router.post("/:id/comments", async (req, res) => {
+  const post_id = Number(req.params.id);
+  const { text } = req.body;
+
+  if (!text)
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+
+  const post = await db.findById(post_id);
+  if (!post.length)
+    return res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." });
+
+  db.insertComment({ post_id, text })
+    .then((id) => res.status(201).json(id))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database",
+      });
+    });
+});
 
 router.get("/", (req, res) => {});
 router.get("/:id", (req, res) => {});
