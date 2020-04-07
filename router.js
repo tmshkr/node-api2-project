589508735a5afa19497f46dc12c8406f1709c933
Tim Shaker <tim@tmshkr.com>
2020-04-07 12:14:39 -0700

    POST /api/posts

--- router.js
+++ router.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const db = require("./data/db");
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!(title && contents)) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+  db.insert({ title, contents })
+    .then((id) => res.status(201).json(id))
+    .catch((err) =>
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      })
+    );
+});
 router.post("/:id/comments", (req, res) => {});
 
 router.get("/", (req, res) => {});
