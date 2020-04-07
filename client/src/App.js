cce23d6accd0edecff9528b1e2fc96dd1aa40d5a
Tim Shaker <tim@tmshkr.com>
2020-04-07 14:37:49 -0700

    fetch lists of posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "./utils/axios";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios.get("/api/posts").then(({ data }) => console.log(data));
+  }, []);
   return <main className="App">hello world</main>;
 }
 
