b455cd3154e3014c3c6c90f6575c8d8462369667
Tim Shaker <tim@tmshkr.com>
2020-04-07 11:58:58 -0700

    import router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const server = express();
+const router = require("./router");
 
 server.use(express.json());
+server.use("/api", router);
 
 server.get("/", (req, res) => {
   res.json({ api: "running..." });
